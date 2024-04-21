d6b7908d10ad7701c7987fba12055599ed3aad6d
mominazaffar <contactmominazafar@gmail.com>
2024-04-22 04:35:36 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = []; //compiling list
+let condition = true;
+while (condition) {
+    let tasks = await inquirer.prompt([
+        {
+            name: "todo",
+            message: "what you want to add in your todo",
+            type: "input"
+        },
+        {
+            name: "addOns",
+            message: "do you want to add more?",
+            type: "confirm",
+            default: "false"
+        }
+    ]);
+    todos.push(tasks.todo);
+    condition = tasks.addOns;
+    console.log(todos);
+}
+;
